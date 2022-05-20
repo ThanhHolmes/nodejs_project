742af1161cb7ae2ba70415349c452a4c0dbc99d9
ThanhHolmes <sherlockthanhnguyen@gmail.com>
2022-05-20 17:36:46 +0700

    Using query parameters

--- index.js
+++ index.js
@@ -3,17 +3,28 @@ const app = express();
 
 const port = 3000;
 
-let  users = [
-    { id: 1, name: "Thành"},
-    { id: 2, name: "Hoàng"}
-]
-
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+let  users = [
+    { id: 1, name: "Thanh"},
+    { id: 2, name: "Hoang"}
+];
+    
     
 app.get("/users", (req, res) => {
     res.render("pages/users", {
         users: users
+    });
+});
+
+app.get("/users/search", (req, res) => {
+    let q = req.query.q;
+    let matchedUsers = users.filter((user) => {
+       return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+    res.render("pages/users", {
+        users: matchedUsers
     });
 });
 app.listen(port, () => console.log(`App listening at port ${port}`));
