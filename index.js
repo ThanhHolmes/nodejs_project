0054f5d8c9a3e39a0fd1b7a4c0b8613bc5a47e74
ThanhHolmes <sherlockthanhnguyen@gmail.com>
2022-05-24 10:05:19 +0700

    Using POST method to add new users

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const port = 3000;
+
+app.use(morgan('combined'));
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 let  users = [
     { id: 1, name: "Thanh"},
-    { id: 2, name: "Hoang"}
+    { id: 2, name: "Hoang"},
+    { id: 3, name: "Sa Lem"},
+    { id: 4, name: "Chung"}
 ];
     
     
@@ -18,6 +28,7 @@ app.get("/users", (req, res) => {
     });
 });
 
+// Using Query Parameters
 app.get("/users/search", (req, res) => {
     let q = req.query.q;
     let matchedUsers = users.filter((user) => {
@@ -27,4 +38,15 @@ app.get("/users/search", (req, res) => {
         users: matchedUsers
     });
 });
+
+// Using POST methods
+app.get("/users/create", (req, res) => {
+    res.render("pages/create");
+});
+
+app.post("/users/create", (req, res) => {
+    users.push(req.body);
+    res.redirect("/users")
+});
+
 app.listen(port, () => console.log(`App listening at port ${port}`));
