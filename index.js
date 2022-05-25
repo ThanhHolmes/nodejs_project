dad69d56a07ac63c9251fe72d1609364e21d252e
ThanhHolmes <sherlockthanhnguyen@gmail.com>
2022-05-25 15:32:17 +0700

    ex

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const morgan = require("morgan");
-const usersRoutes = require("./routes/users.route");
+const morgan = require('morgan');
+const usersRoutes = require('./routes/users.route');
 
 const port = 3000;
 
 app.use(morgan('combined'));
 
-app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 app.use(express.json());
 
 // Route init
-app.use("/users", usersRoutes);
+app.use('/users', usersRoutes);
 
 app.listen(port, () => console.log(`App listening at port ${port}`));
