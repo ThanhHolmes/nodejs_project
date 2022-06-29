21ada28f54100a302469097b6aada9e3e13f4669
ThanhHolmes <sherlockthanhnguyen@gmail.com>
2022-06-29 15:32:42 +0700

    do account authentication

--- routes/class.route.js
+++ routes/class.route.js
@@ -2,15 +2,26 @@ const express = require('express');
 const router = express.Router();
 const classController = require('../controllers/class.controller');
 const classValidate = require('../validate/class.validate');
+const authController = require('../controllers/auth.controller')
 
-router.get('/classes', classController.showClass);
+// router.get('/classes', classController.showClass);
+router.route('/classes')
+    .get(authController.checkLoggedIn, classController.showClass);
 
-router.get('/classes/:id/edit', classController.editClass);
+// router.get('/classes/:id/edit', classController.editClass);
+router.route('/classes/:id/edit')
+    .get(authController.checkLoggedIn, classController.editClass);
 
-router.post('/classes/:id/update', classValidate.updateClass, classController.updateClass);
+// router.post('/classes/:id/update', classValidate.updateClass, classController.updateClass);
+router.route('/classes/:id/update')
+    .post(authController.checkLoggedIn, classValidate.updateClass, classController.updateClass);
+    
+// router.get('/classes/:id/delete', classController.deleteClass);
+router.route('/classes/:id/delete')
+    .get(authController.checkLoggedIn, classController.deleteClass);
 
-router.get('/classes/:id/delete', classController.deleteClass);
-
-router.get('/classes/:id/students', classController.showStudentList);
+// router.get('/classes/:id/students', classController.showStudentList);
+router.route('/classes/:id/students')
+    .get(authController.checkLoggedIn, classController.showStudentList);
 
 module.exports = router;
--- routes/student.route.js
+++ routes/student.route.js
@@ -3,12 +3,20 @@ const router = express.Router();
 const studentController = require('../controllers/student.controller');
 const studentValidate = require('../validate/student.validate');
 
-router.post('/student/add_newstudent', studentValidate.addData,  studentController.addData);
+// router.post('/student/add_newstudent', studentValidate.addData,  studentController.addData);
+router.route('/student/add-newstudent')
+    .post(studentValidate.addData, studentController.addData);
 
-router.get('/student/:id/edit-student', studentController.editStudent);
+// router.get('/student/:id/edit-student', studentController.editStudent);
+router.route('/student/:id/edit-student')
+    .get(studentController.editStudent);
 
-router.post('/student/:id/update-student', studentValidate.updateStudent, studentController.updateStudent);
+// router.post('/student/:id/update-student', studentValidate.updateStudent, studentController.updateStudent);
+router.route('/student/:id/update-student')
+    .post(studentValidate.updateStudent, studentController.updateStudent);
 
-router.get('/student/:id/delete-student', studentController.deleteStudent);
+// router.get('/student/:id/delete-student', studentController.deleteStudent);
+router.route('/student/:id/delete-student')
+    .get(studentController.deleteStudent);
 
 module.exports = router;
